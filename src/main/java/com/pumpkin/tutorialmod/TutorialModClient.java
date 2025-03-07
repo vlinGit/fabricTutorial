8810cb926762f54ae3314bbabdf9049288ee9a5d
Vincent Lin <vincent@pockyt.io>
2025-03-06 19:28:01 -0500

    Initial commit

---
+++ src/main/java/com/pumpkin/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package com.pumpkin.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
